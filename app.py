cc2047fbcc88311f9507ccce6f604b41188cb780
Eswar Aditya <142793639+EswarAditya5@users.noreply.github.com>
2024-03-08 15:25:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PIL import Image
 import numpy as np
 
 # Load the pre-trained model
-model = tf.keras.models.load_model("shoe_classification_model.h5")
+model = load_model("shoe_classification_model.h5")
 
 # Define the shoe categories
 categories = ['Boots', 'Sandals', 'Slippers']  # Update with your categories
